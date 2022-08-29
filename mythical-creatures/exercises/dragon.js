167eb17a181cebb4f82f24566bd190644c77585a
Sam Rice <samsrice@gmail.com>
2022-08-28 21:23:47 -0700

    Initial commit

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name,
+        this.rider = rider,
+        this.hungry = true,
+        this.meals = 0
+    };
 
+    greet() {
+        return `Hi, ${this.rider}!`;
+    };
+
+    eat() {
+        this.meals++;
+        if (this.meals === 3) {
+            this.hungry = false;
+            return this.hungry;
+        };
+    };
+};
+
+
+module.exports = Dragon;
--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,15 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
+  }
+
+  isWhite() {
+    return this.color === "white";
+  };
+
+  says(text) {
+    return `**;* ${text} *;**`
   }
 }
 
